3e2fb83c34c0a0c1026d9636ca735cbfca97538e
Angel Roldán <angel.roldan@km77.com>
2023-08-10 08:09:23 +0200

    Gets the directory path from a script parameter

--- run-files.py
+++ run-files.py
@@ -1,5 +1,6 @@
 import os
 import re
+import argparse
 
 def check_file_names(root_dir):
     pattern = re.compile(r'^[0-9]{4}-(0[1-9]|1[0-2])-([0-2][0-9]|3[01]) [0-2][0-9]\.[0-5][0-9]\.[0-5][0-9]\.(jpg|png|mp4|avi)$')
@@ -9,5 +10,13 @@ def check_file_names(root_dir):
             if not pattern.match(filename):
                 print(f"Invalid filename: {filename}")
 
-root_directory = ""
-check_file_names(root_directory)
+def main():
+    parser = argparse.ArgumentParser(description="Check all files recursively to find those that do not match the desired name structure")
+    parser.add_argument("--directory", required=True, help="Directory path to check")
+    args = parser.parse_args()
+
+    root_directory = args.directory
+    check_file_names(root_directory)
+
+if __name__ == "__main__":
+    main()
